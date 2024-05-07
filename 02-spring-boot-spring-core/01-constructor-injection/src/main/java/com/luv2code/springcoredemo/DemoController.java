afe9ae1f5d4c9b000a749c3119789cca1f34d286
Mahmoud Abdelaal <mahmoud.abdelaal.dev@gmail.com>
2024-05-07 14:38:19 +0300

    Section 02

---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/DemoController.java
@@ -0,0 +1,23 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+    private Coach mycoach;
+
+    // define a constructor for dependency injection
+
+    @Autowired
+    public DemoController(Coach thecoach){
+        mycoach=thecoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return mycoach.getDailyWorkout();
+    }
+}
---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,30 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class DemoController {
+    // define a private field for the dependency
+    private Coach mycoach;
+
+    public DemoController() {
+        System.out.println("In Constructor "+getClass().getSimpleName());
+    }
+// define a constructor for dependency injection
+
+@Autowired
+public void DemoController(@Qualifier("cricketCoach") Coach thecoach){
+    mycoach=thecoach;
+}
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return mycoach.getDailyWorkout();
+    }
+}
